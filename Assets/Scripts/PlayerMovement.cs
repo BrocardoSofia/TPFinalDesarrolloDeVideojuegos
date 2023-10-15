a120df4e18b4fedc84522fb33909c3fbf1fb8d4d
BrocardoSofia <sofia.brocardo@hotmail.com>
2023-10-15 17:00:41 -0300

    appearing animation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -15,7 +15,7 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private float jumpForce = 14f;
 
-    private enum MovementState { idle, running, jumping, falling }
+    private enum MovementState { idle, running, jumping, falling, appearing, desaesappearing }
     private MovementState state;
 
     // Start is called before the first frame update
