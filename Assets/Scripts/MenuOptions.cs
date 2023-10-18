71dc4a3a49dfd2ed0054892a29978d317c0efffd
BrocardoSofia <sofia.brocardo@hotmail.com>
2023-10-18 14:26:06 -0300

    button quit

---
+++ Assets/Scripts/MenuOptions.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuOptions : MonoBehaviour
+{
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
