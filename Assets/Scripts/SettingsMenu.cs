e71f455eabcdbf4f4bcf420615e5ed88a893837f
BrocardoSofia <sofia.brocardo@hotmail.com>
2023-10-18 18:20:53 -0300

    resolutions settings

--- Assets/Scripts/SettingsMenu.cs
+++ Assets/Scripts/SettingsMenu.cs
@@ -2,13 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour
 {
-
     [SerializeField] private AudioMixer audioMixer;
+    [SerializeField] private Dropdown resolutionsDropDown;
+
+    Resolution[] resolutions;
+
+    private void Start()
+    {
+        string option;
+        List<string> options = new List<string>();
+
+        resolutions = Screen.resolutions;
+        resolutionsDropDown.ClearOptions();       
+
+        for(int i=0; i<resolutions.Length; i++)
+        {
+            option = resolutions[i].width + "x" + resolutions[i].height;
+            options.Add(option);
+        }
+
+        resolutionsDropDown.AddOptions(options);
+    }
+
     public void SetVolume(float volume)
     {
         audioMixer.SetFloat("volume", volume);
+    }
+
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetFullScreen(bool isFullScreen)
+    {
+        Screen.fullScreen = isFullScreen;
     }
 }
